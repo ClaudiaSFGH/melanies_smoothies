107ae824b3df77a49ec5b8d1900ef8930ed7d1d6
ClaudiaSFGH <ccarpinteroborrajo@deloitte.es>
2025-01-04 15:23:15 +0100

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+#my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+#st.write(my_dataframe)
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients:"
+    , my_dataframe
+    , max_selections = 5
+    )
+#st.write(ingredients_list)
+if ingredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        
+    st.write(ingredients_string)
+    
+    my_insert_stmt = """ insert into smoothies.public.orders(INGREDIENTS, NAME_ON_ORDER)
+     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+    #st.write(my_insert_stmt)
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+
+#editable_df = st.data_editor(my_dataframe)
+
