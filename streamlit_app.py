1a1dd8199cdd8ccd0bf59885ed83eea88a619aeb
ClaudiaSFGH <ccarpinteroborrajo@deloitte.es>
2025-01-05 12:58:53 +0100

    Update streamlit_app.py
    nutrition information for each and every selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
 import requests
 
 
@@ -31,6 +30,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
         
     st.write(ingredients_string)
     
@@ -43,9 +44,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
         
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
+
 
 #editable_df = st.data_editor(my_dataframe)
 
