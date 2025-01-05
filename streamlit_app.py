5151eebcbe577db98d892bc8d150ad28f074727a
ClaudiaSFGH <ccarpinteroborrajo@deloitte.es>
2025-01-05 12:56:51 +0100

    Update streamlit_app.py
    Datafram with the API respone

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smooothie :cup_with_straw:")
@@ -43,6 +42,10 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+        
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
 
 #editable_df = st.data_editor(my_dataframe)
 
